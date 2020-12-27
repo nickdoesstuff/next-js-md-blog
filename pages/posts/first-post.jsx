7b5a7d81ca617783aa97aed0a759e0cc8ae26ae4
Nick Shelton <nickshelton1376@gmail.com>
2020-12-27 15:34:19 -0500

    add _app, update layout, add base css

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Head from "next/head";
-import React from "react";
+
+import Layout from "../../components/layout";
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -14,7 +15,7 @@ const FirstPost = () => {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 };
 

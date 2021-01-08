b05e167c583ce3a3123da64dc5b6b35ff6dfe75b
Nick Shelton <nickshelton1376@gmail.com>
2021-01-08 09:43:15 -0500

    remove old scrolling package

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,12 @@
 import AnimatedPageWrapper from "../components/AnimatedPageWrapper";
 import TopNav from "../components/site/TopNav";
 import PageProgress from "../components/site/PageProgress";
-import smoothscroll from "smoothscroll-polyfill";
 
 import { AnimatePresence } from "framer-motion";
 
 import "../styles/_app.scss";
 
 const App = ({ Component, pageProps, router }) => {
-  //add smooth scrolling support
-  React.useEffect(() => {
-    smoothscroll.polyfill();
-    console.log("smooth");
-  }, []);
-
   return (
     <>
       <PageProgress page={router.route} />
